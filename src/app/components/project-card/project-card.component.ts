50b05c00296cfe1f87b226a71c198ddc5f51ba4e
MiguelAngelLopGar <59963705+MiguelAngelLopezGarcia@users.noreply.github.com>
2025-06-20 13:21:22 +0200

    Added links to buttons and embeded videos

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,11 +13,23 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule]
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnChanges {
   @Input() title: string = '';
   @Input() description: string = '';
   @Input() buttonText: string = '';
   @Input() previewText: string = '';
   @Input() technologies: string[] = [];
   @Input() backgroundButton: string = '';
+  @Input() link: string = '';
+  @Input() videoLink: string = '';
+
+  safeVideoUrl: SafeResourceUrl = '';
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['videoLink'] && this.videoLink) {
+      this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoLink);
+    }
+  }
 }
